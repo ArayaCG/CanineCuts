518b3f3125abd114b86b6ae24741922225fb8f3a
ArayaCG <arayag64@gmail.com>
2024-04-16 17:35:57 -0300

    intento form

--- front/pm3-project/src/views/Register/Register.jsx
+++ front/pm3-project/src/views/Register/Register.jsx
@@ -1,7 +1,51 @@
-const Register = () =>{
+import { useState } from "react";
+
+const Register = () => {
+    const initialState = {
+        name: "",
+        email: "",
+        birthdate: "",
+        nDni: "",
+        username: "",
+        password: "",
+    };
+    const { form, setForm } = useState({});
+    const { errors, setError } = useState({});
+
+    const handleChange = () => {
+        
+    }
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <div>Register</div>
-    )
-}
+        <div>
+            <h2>User Registration</h2>
+            <form onSubmit={handleSubmit}>
+                {
+                    [
+                        {label: "Name", name: "name", type:"text"},
+                        {label: "Email", name: "email", type:"text"},
+                        {label: "Username", name: "username", type:"text"},
+                        {label: "Password", name: "password", type:"password"},
+                        {label: "N° DNI", name: "nDni", type:"text"},
+                        {label: "Birthdate", name: "birthdate", type:"date"},
+                    ].map(({name, label, type})=>{
+                        return (
+                            <div key={name}> 
+                                <label>{label}</label>
+                                <input type={type} onChange={handleChange} name={name} value={form[name]}/>
+                                {errors[name] && <span key={name}>{errors[name]}</span>}
+                            </div>
+                        )
+                    })
+                }
+                <button type="submit">Register</button>
+            </form>
+        </div>
+        );
+};
 
 export default Register;
